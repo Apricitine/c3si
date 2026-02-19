e352f0b8723c94176ecaf525466a7559f9bc116c
Jeo Lee <jeo51810@gmail.com>
2026-02-19 15:44:15 -0800

    changed website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+	
+<div class="border"></div>
+
+<div class="content">
+    <h1 class="info">C3 App / List of summer programs & volunteering opportunities</h1>
+
+    <table>
+        <tbody>
+            <tr>
+                <th>Name 1</th>
+                <td>Description 1</td>
+            </tr>
+            <tr>
+                <th>Name 2</th>
+                <td>Description 2</td>
+            </tr>
+            <tr>
+                <th>Name 3</th>
+                <td>Description 3</td>
+            </tr>
+            <tr>
+                <th>Name 4</th>
+                <td>Description 4</td>
+            </tr>
+            <tr>
+                <th>Name 5</th>
+                <td>Description 5</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+<div class="end"></div>
+
+<p>Info</p>
