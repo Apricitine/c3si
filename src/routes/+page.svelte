aeec139aad8c5a500994f7ba7ffc2143498c0602
kenny lee <kenjlee09@gmail.com>
2026-02-19 16:02:40 -0800

    reformatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,43 +2,25 @@
   let { data } = $props();
 </script>
 
-<ul>
-  {#each data.scholarships as scholarship}
-    <li><a href={scholarship.primary_link}>{scholarship.name}</a> - {scholarship.description}</li>
-  {/each}
-</ul>
-	
 <div class="border"></div>
 
 <div class="content">
     <h1 class="info">C3 App / List of summer programs & volunteering opportunities</h1>
-
-    <table>
-        <tbody>
-            <tr>
-                <th>Name 1</th>
-                <td>Description 1</td>
-            </tr>
-            <tr>
-                <th>Name 2</th>
-                <td>Description 2</td>
-            </tr>
-            <tr>
-                <th>Name 3</th>
-                <td>Description 3</td>
-            </tr>
-            <tr>
-                <th>Name 4</th>
-                <td>Description 4</td>
-            </tr>
-            <tr>
-                <th>Name 5</th>
-                <td>Description 5</td>
-            </tr>
-        </tbody>
-    </table>
+    <ul>
+    {#each data.scholarships as scholarship}
+        <li><a href={scholarship.primary_link}>{scholarship.name}</a> - {scholarship.description}</li>
+    {/each}
+    </ul>
 </div>
 
 <div class="end"></div>
 
 <p>Info</p>
+
+
+
+<style lang="scss">
+    
+
+
+</style>
