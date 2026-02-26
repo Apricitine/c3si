707f9c3d800f757e68b2d7c3a609edb9577ec808
kenny lee <kenjlee09@gmail.com>
2026-02-26 15:42:03 -0800

    stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,4 +8,20 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<div class="header">
+	<div class="content">
+    	<h1 class="info">C3 App / List of summer programs & volunteering opportunities</h1>
+		<div class="routes">
+			<a class="route-link" href="./Scholarships">Scholarships</a>
+			<a class="route-link" href="./Internships">Internships</a>
+		</div>
+	</div>
+</div>
+
 {@render children()}
+
+
+
+<style lang="scss">
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,18 +4,15 @@
 
 <div class="border"></div>
 
-<div class="content">
-    <h1 class="info">C3 App / List of summer programs & volunteering opportunities</h1>
-    <ul>
-    {#each data.scholarships as scholarship}
-        <li><a href={scholarship.primary_link}>{scholarship.name}</a> - {scholarship.description}</li>
-    {/each}
-    </ul>
-</div>
+
 
 <div class="end"></div>
 
-<p>Info</p>
+<ul>
+{#each data.scholarships as scholarship}
+    <li><a href={scholarship.primary_link}>{scholarship.name}</a> - {scholarship.description}</li>
+{/each}
+</ul>
 
 
 
