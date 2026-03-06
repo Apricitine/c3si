fddeea75d040267f1e5b8452a5fb1cffafa0ee78
kenny lee <kenjlee09@gmail.com>
2026-03-05 21:55:53 -0800

    tags implimented

---
+++ src/lib/components/Tag.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+
+
+    const colorHexes = {
+        cyan: "#00FFFF",
+        red: "#FF0000",
+        green: "#008000",
+        yellow: "#FFEA00",
+        orange: "#FF8C00",
+        purple: "#9F2B6"
+    }
+
+    type Color = keyof typeof colorHexes;
+
+    interface Tag {
+        color: Color
+        name: string
+    }
+
+    let { color, name }: Tag = $props()
+
+</script>
+
+
+
+<div class="tag" style={`background-color: ${colorHexes[color]}`}>
+    {name}
+</div>
+
+
+<style lang="scss">
+    .tag {
+        position: flex;
+
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,7 @@
-<script lang="ts">
-  import Scholarship from "$lib/components/Scholarship.svelte"
+<script>
+  import Tag from "$lib/components/Tag.svelte"
 
-  let { data } = $props()
-
-  let formatDate = (date: Date) => `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
-  let noZero = (str: string): string => str.replace(/^0+(?!$)/, "")
 </script>
 
-<div class="border"></div>
 
-<div class="end"></div>
-
-
-  {#each data.scholarships as scholarship}
-    <Scholarship name={scholarship.name} deadline={formatDate(new Date(scholarship.deadline))} description={scholarship.description}/>
-  {/each}
-
-
-<style lang="scss">
-</style>
+<Tag color="cyan" name = "bingbong" ></Tag>
