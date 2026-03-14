f53c724b457a8890df2687a8fe17b2e0a3173e63
kenny lee <kenjlee09@gmail.com>
2026-03-14 15:41:19 -0700

    Implementing search algorithm

---
+++ src/lib/components/Search.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let searchTerm: string
+</script>
+
+<div class="search-input-cont">
+    <input type="text"
+        class="search-field"
+        placeholder="Enter Search Term"
+        autocomplete="off"
+        bind:value={searchTerm}
+        on:input />
+</div>
+
+
+<style lang="scss">
+    .search-input-cont {
+        width:40%;
+        display: flex;
+        align-items: center;
+        margin: 0 0 0 10vw;
+    }
+
+</style>
--- src/routes/scholarships/+page.svelte
+++ src/routes/scholarships/+page.svelte
@@ -3,17 +3,27 @@
     import Modal from "$lib/components/Modal.svelte"
     import Scholarship from "$lib/components/Scholarship.svelte"
     import Tag from "$lib/components/Tag.svelte"
+    import Search from "$lib/components/Search.svelte"
+    import stringSimilarity from "$lib/scripts/Regex"
     
-    let { data }: PageServerData = $props()
+    let { data }: PageServerData = $props() satisfies Scholarship[]
     let showModal = $state(false)
+    let searchTerm = $state("");
+
+    let goodData = data as Scholarship[]
 
     let formatDate = (date: Date) =>
         `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
     let noZero = (str: string): string => str.replace(/^0+(?!$)/, "")
 
-    console.log(data)
+    const searchSchols = () => {
+
+    }
+    
+
 </script>
 
+<Search bind:searchTerm on:input />
 {#each data.scholarships as scholarship}
     <Scholarship onclick={() => (showModal = true)}
         name={scholarship.name}
